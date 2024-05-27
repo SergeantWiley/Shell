7bc5f9f043ae24a913f6ac68c7109c386e117c46
Corporal Rat <86330761+SergeantWiley@users.noreply.github.com>
2024-05-26 19:06:08 -0700

    Add files via upload

--- boot.sh
+++ boot.sh
@@ -1,2 +1,8 @@
-python game.py
-pause
+read -p "Want to play some RPS?: " entry
+# Check the user's input
+if [ "$entry" = "yes" ]; then
+    python game.py
+else
+    echo "Dang, Not today I guess."
+fi
+read -p "Press Enter to exit..."
--- game.py
+++ game.py
@@ -1,36 +1,38 @@
 import random
-
-index = {"Rock": 0,"Paper": 1, "Scissors": 2}
-inverse_index = {0: "Rock", 1: "Paper", 2: "Scissors"}
-computer = random.randint(0,2)
-player = input("Enter one of Rock, Paper, or Scissors: ")
-player = index[player]
-if player == computer:
-    print("Draw")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
-elif player == 0 and computer == 1:
-    print("You lose")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
-elif player == 0 and computer == 2:
-    print("You win")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
-elif player == 1 and computer == 2:
-    print("You lose")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
-elif player == 1 and computer == 0:
-    print("You win")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
-elif player == 2 and computer == 0:
-    print("You lose")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
-elif player == 2 and computer == 1:
-    print("You win")
-    print("Computer picked:", inverse_index[computer])
-    print("You picked:", inverse_index[player])
+def game():
+    index = {"Rock": 0,"Paper": 1, "Scissors": 2}
+    inverse_index = {0: "Rock", 1: "Paper", 2: "Scissors"}
+    computer = random.randint(0,2)
+    player = input("Enter one of Rock, Paper, or Scissors: ")
+    player = index[player]
+    if player == computer:
+        print("Draw")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    elif player == 0 and computer == 1:
+        print("You lose")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    elif player == 0 and computer == 2:
+        print("You win")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    elif player == 1 and computer == 2:
+        print("You lose")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    elif player == 1 and computer == 0:
+        print("You win")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    elif player == 2 and computer == 0:
+        print("You lose")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    elif player == 2 and computer == 1:
+        print("You win")
+        print("Computer picked:", inverse_index[computer])
+        print("You picked:", inverse_index[player])
+    game()
+game()
 
